1cc377b2a0fcb4b3420e25d694507525f7f89b89
unknown <raoyun@posbao.net>
2022-04-29 17:28:59 +0800

    注册接口“

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,7 @@
 # -- coding: utf-8 --
 from flask import Flask
 from config import Config
-from app.controller.auth.user import auth
+
 
 yolo = Flask(__name__)
-yolo.config.from_object(Config)
-
-# 注册蓝图
-yolo.register_blueprint(auth)
-
 yolo.config.from_object(Config)
--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 # -- coding: utf-8 --
 from app import yolo
-from app.utils.log import Log
+from app.utils.logger import Log
 from app import dao
-
+from app.controller.auth.user import auth
+# 注册蓝图
+yolo.register_blueprint(auth)
 
 @yolo.route('/')
 def hello_world():
